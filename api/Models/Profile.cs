56c292d4e9b1d7878349735269ca4e4d96b92002
AmirPorm <promgrameramir@gmail.com>
2025-06-09 00:27:40 +0330

    MOdel Profil

--- api/Models/Profile.cs
+++ api/Models/Profile.cs
@@ -1,6 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace api.Models;
 
-public class Profile
-{
-    
-}
+public record Profile
+(
+    [property: BsonId, BsonRepresentation(BsonType.ObjectId)]
+    string? Id,
+    [Length(24, 24)] string UserId,
+    string ROle,
+    string Bio,
+    string Skills,
+    string Avatar,
+    DateTime CreatedAt
+
+);

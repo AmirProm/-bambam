8121c0a4f23369da24ec9161c4f3cd58fab27520
AmirPorm <promgrameramir@gmail.com>
2025-06-13 01:29:47 +0330

    Add Controler

---
+++ api/DTOs/AccountController.cs
@@ -0,0 +1,49 @@
+using api.Interfaces;
+using api.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace api.DTOs;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountController(IAccountRepository accountRepository) : ControllerBase
+{
+    
+
+    [HttpPost("register")] // List<AppUser> appUsers = await _collection.Find(new BsonDocument()).ToListAsync(cancellationToken);
+    public async Task<ActionResult<Loggind>> Register(AppUser userInput, CancellationToken cancellationToken)
+    {
+        if (userInput.Password != userInput.ConfirmPassword)
+            return BadRequest("Passsword mach ne dorst kon");
+
+        Loggind? loggedInDto = await accountRepository.RegisterAsync(userInput, cancellationToken);
+
+        if (loggedInDto is null)
+            return BadRequest("Email alredy dorst kon");
+
+        return Ok(loggedInDto);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<Loggind>> Login(LoginDto userInput, CancellationToken cancellationToken)
+    {
+        Loggind? loggedInDto = await accountRepository.LoginAsync(userInput, cancellationToken);
+
+        if (loggedInDto is null)
+            return BadRequest("Email ya Password estebah");
+
+        return loggedInDto;
+    }
+
+    [HttpDelete("delete/{userId}")]
+    public async Task<ActionResult<DeleteResult>> DeleteById(string userId, CancellationToken cancellationToken)
+    {
+        DeleteResult? deleteResult = await accountRepository.DeleteByIdAsync(userId, cancellationToken);
+
+        if (deleteResult is null)
+            return BadRequest("ID namojod");
+
+        return deleteResult;
+    }
+}

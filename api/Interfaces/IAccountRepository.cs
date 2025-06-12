90819c2a1171c0dab0afc177feee7eca8177349a
AmirPorm <promgrameramir@gmail.com>
2025-06-12 22:32:15 +0330

    IAccountRepository

--- api/Interfaces/IAccountRepository.cs
+++ api/Interfaces/IAccountRepository.cs
@@ -1,11 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using api.DTOs;
+using api.Models;
+using MongoDB.Driver;
 
 namespace api.Interfaces;
 
 public interface IAccountRepository
 {
-    
+    public Task<Loggind?> RegisterAsync(AppUser userInput, CancellationToken cancellationToken);
+    public Task<Loggind?> LoginAsync(LoginDto userInput, CancellationToken cancellationToken);
+    public Task<AppUser?> UpdataAsync(string userId, CancellationToken cancellationToken);
+    public Task<DeleteResult?> DeleteByIdAsync(string userId, CancellationToken cancellationToken);
 }

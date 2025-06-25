ce4c241133ddcacf019adad601a269f013787810
AmirPorm <promgrameramir@gmail.com>
2025-06-25 23:00:56 +0330

    add controler api.csproj

--- api/Controller/AccountController.cs
+++ api/Controller/AccountController.cs
@@ -23,27 +23,27 @@ public class AccountController(IAccountRepository accountRepository) : Controlle
             return BadRequest("This email is already taken.");
 
         return Ok(loggedInDto);
-    }
+    // }
 
-    [HttpPost("login")]
-    public async Task<ActionResult<Loggind>> Login(LoginDto userInput, CancellationToken cancellationToken)
-    {
-        Loggind? loggedInDto = await accountRepository.LoginAsync(userInput, cancellationToken);
+    // [HttpPost("login")]
+    // public async Task<ActionResult<Loggind>> Login(LoginDto userInput, CancellationToken cancellationToken)
+    // {
+    //     Loggind? loggedInDto = await accountRepository.LoginAsync(userInput, cancellationToken);
 
-        if (loggedInDto is null)
-            return BadRequest("Email or Password is wrong");
+    //     if (loggedInDto is null)
+    //         return BadRequest("Email or Password is wrong");
 
-        return loggedInDto;
-    }
+    //     return loggedInDto;
+    // }
 
-    [HttpDelete("delete/{userId}")]
-    public async Task<ActionResult<DeleteResult>> DeleteById(string userId, CancellationToken cancellationToken)
-    {
-        DeleteResult? deleteResult = await accountRepository.DeleteByIdAsync(userId, cancellationToken);
+    // [HttpDelete("delete/{userId}")]
+    // public async Task<ActionResult<DeleteResult>> DeleteById(string userId, CancellationToken cancellationToken)
+    // {
+    //     DeleteResult? deleteResult = await accountRepository.DeleteByIdAsync(userId, cancellationToken);
 
-        if (deleteResult is null)
-            return BadRequest("Operation failed");
+    //     if (deleteResult is null)
+    //         return BadRequest("Operation failed");
 
-        return deleteResult;
-    }
+    //     return deleteResult;
+    // }
 }

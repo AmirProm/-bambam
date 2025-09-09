af14b6cdff65359fb72d826d82d90c0daafceb28
AmirPorm <promgrameramir@gmail.com>
2025-09-09 21:26:32 +0330

    interface account relodloggedin

--- api/Interfaces/IAccountRepository.cs
+++ api/Interfaces/IAccountRepository.cs
@@ -4,8 +4,7 @@ public interface IAccountRepository
 {
     public Task<LoggInDto?> RegisterAsync(AppUser userInput, CancellationToken cancellationToken);
     public Task<LoggInDto?> LoginAsync(LoginDto userInput, CancellationToken cancellationToken);
-
-    // public Task<AppUser?> UpdataAsync(string userId, CancellationToken cancellationToken);
-    
     public Task<DeleteResult?> DeleteByIdAsync(string userId, CancellationToken cancellationToken);
+    public Task<LoggInDto?> ReloadLoggedInUserAsync(string userId, string token, CancellationToken cancellationToken);
+
 }
